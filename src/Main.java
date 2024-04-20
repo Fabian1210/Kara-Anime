beec766b39a64be18a97b7872337c4915fbda7a1
Fabian Tovar <thefabiantovar@gmail.com>
2024-04-20 14:00:59 -0700

    new name and project purpose. new initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("For me giving up is way harder than trying \n- Kanye");
     }
 }
